78c0ecac1feba479f156b8e3e34c1b88707392d4
MaxMorehead <moreheadmax@gmail.com>
2016-06-27 16:44:55 -0400

    fixed softlinkTickets.py

--- scripts/softlinkTickets.py
+++ scripts/softlinkTickets.py
@@ -3,7 +3,8 @@
 
 import os
 
-for directoryName in os.listdir("../IndividualNovae"):
-	for ticketType in ["CompletedTickets", "PendingTickets"]:	
-		for fileName in os.list("../IndividualNovae/" + directoryName + "/Tickets/" + ticketType):
-			os.symlink("../Tickets/" + ticketType + "/" + fileName, "../IndividualNovae/" + directoryName + "/Tickets/" + ticketType + "/" + fileName)
+for directoryName in os.listdir("../Individual_Novae"):
+	if "." not in directoryName:
+		for ticketType in ["CompletedTickets", "PendingTickets"]:	
+			for fileName in os.listdir("../Individual_Novae/" + directoryName + "/Tickets/" + ticketType):
+				os.symlink("../Tickets/" + ticketType + "/" + fileName, "../Individual_Novae/" + directoryName + "/Tickets/" + ticketType + "/" + fileName)

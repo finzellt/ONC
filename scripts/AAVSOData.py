05af3210a3209582f7f0f9ca206ac9ad856ec0a6
Daniel Coulter <coulte35@msu.edu>
2016-07-07 14:09:18 -0400

    created a script to format AAVSO data files properly

---
+++ scripts/AAVSOData.py
@@ -0,0 +1,37 @@
+import os
+
+# for file_name in os.listdir("Data"):
+file_name = input("AAVSO Data Filename: ")
+data = open(file_name, "r")
+
+string = []
+while True:
+	
+	flag = 0
+	line = data.readline()
+	if not line:
+		break
+	cols = line.split(",")
+	
+	try:
+		for char in cols[1]:
+			if char == "<":
+				flag += 1
+	except IndexError:
+		continue
+
+	if flag == 1:
+		cols[1] += ",1"
+	else:
+		cols[1] += ",0"
+	
+	line = ",".join(cols)
+	string.append(line)
+
+data.close()
+
+string = "".join(string)
+
+data = open(file_name, "w")
+data.write(string)
+data.close()
